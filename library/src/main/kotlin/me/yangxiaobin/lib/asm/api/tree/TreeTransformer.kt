418b424d3c6b4fbe665f55069346033434c41c6c
yangxiaobin <yangxiaobin@kuaishou.com>
2021-10-09 11:15:07 +0800

    Add tree api.

---
+++ library/src/main/kotlin/me/yangxiaobin/lib/asm/api/tree/TreeTransformer.kt
@@ -0,0 +1,30 @@
+@file:Suppress("MemberVisibilityCanBePrivate")
+
+package me.yangxiaobin.lib.asm.api.tree
+
+import org.objectweb.asm.tree.ClassNode
+import org.objectweb.asm.tree.FieldNode
+import org.objectweb.asm.tree.MethodNode
+
+open class TreeClassTransformer(private val tct: TreeClassTransformer?) {
+
+    fun transform(cn: ClassNode) {
+        tct?.transform(cn)
+    }
+}
+
+
+open class TreeMethodTransformer(private val tmt: TreeMethodTransformer?) {
+
+    fun transform(mn: MethodNode) {
+        tmt?.transform(mn)
+    }
+}
+
+open class TreeFieldTransformer(private val tft: TreeFieldTransformer?) {
+
+    fun transform(fn: FieldNode) {
+        tft?.transform(fn)
+
+    }
+}
