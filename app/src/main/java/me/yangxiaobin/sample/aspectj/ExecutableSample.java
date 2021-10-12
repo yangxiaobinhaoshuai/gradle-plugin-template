d999d4fed2fcaf66866c37d92dfdea10ca900b7c
yangxiaobin <yangxiaobin@kuaishou.com>
2021-10-12 21:43:15 +0800

    Change executable example.

--- app/src/main/java/me/yangxiaobin/sample/aspectj/ExecutableSample.java
+++ app/src/main/java/me/yangxiaobin/sample/aspectj/ExecutableSample.java
@@ -2,7 +2,13 @@ package me.yangxiaobin.sample.aspectj;
 
 class ExecutableSample {
 
-    public void execute () {
-        System.out.println("----> execute");
+    public void execute() {
+        System.out.println("----> ExecutableSample executed");
+    }
+
+
+    public static void main(String[] args) {
+        ExecutableSample demo = new ExecutableSample();
+        demo.execute();
     }
 }
