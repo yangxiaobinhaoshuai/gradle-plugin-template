8eb28095d7f8a284e7dfb04afa463f444e413473
yangxiaobin <yangxiaobin@kuaishou.com>
2021-10-13 19:32:44 +0800

    Add java ext.

--- library/src/main/kotlin/me/yangxiaobin/lib/ext/JavaSystemExt.kt
+++ library/src/main/kotlin/me/yangxiaobin/lib/ext/JavaSystemExt.kt
@@ -2,4 +2,7 @@ package me.yangxiaobin.lib.ext
 
 
 val currentWorkPath: String = System.getProperty("user.dir")
+
 val currentUserHomePath: String = System.getProperty("user.home")
+
+val currentJreClasspath = System.getProperty("sun.boot.class.path")
