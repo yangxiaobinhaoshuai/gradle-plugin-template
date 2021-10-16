98aedf29ca7c4723f501843076e2afa11eed7fd0
yangxiaobin <yangxiaobinn@gmail.com>
2021-10-16 13:24:43 +0800

    Complish groovy aspectj plugin.

--- buildSrc/src/main/groovy/me/yangxaobin/buildsrc/groovy/GroovyAspectJPlugin.groovy
+++ buildSrc/src/main/groovy/me/yangxaobin/buildsrc/groovy/GroovyAspectJPlugin.groovy
@@ -1,13 +1,15 @@
 package me.yangxaobin.buildsrc.groovy
 
+import me.yangxiaobin.lib.base.BasePlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.execution.TaskExecutionGraph
+import org.gradle.api.file.DuplicatesStrategy
 import org.gradle.api.logging.Logger
 import org.gradle.api.tasks.compile.AbstractCompile
 
-class GroovyAspectJPlugin implements Plugin<Project> {
+class GroovyAspectJPlugin extends BasePlugin {
 
     private static final TAG = "GAP"
 
@@ -22,17 +24,27 @@ class GroovyAspectJPlugin implements Plugin<Project> {
 
         resolveAspectJJrtClasspath(target)
 
-        setupAspectJCompilerArgs(target)
+        configJarTask(target)
 
+        setupAspectJCompilerArgs(target)
     }
 
     private void resolveAspectJJrtClasspath(Project project) {
 
-        project.configurations {
-            ajc
-        }
+        project.configurations { ajc }
 
-        project.dependencies.add("ajc","org.aspectj:aspectjtools:1.9.7")
+        project.dependencies.add("ajc", "org.aspectj:aspectjtools:1.9.7")
+    }
+
+
+    private void configJarTask(Project project) {
+
+        project.gradle.taskGraph.whenReady { TaskExecutionGraph graph ->
+
+            graph.allTasks.find { it.name.contains("jar") }?.configure {
+                duplicatesStrategy DuplicatesStrategy.EXCLUDE
+            }
+        }
     }
 
     private void setupAspectJCompilerArgs(Project project) {
@@ -42,37 +54,45 @@ class GroovyAspectJPlugin implements Plugin<Project> {
             List<AbstractCompile> compiles = graph.allTasks.findAll { it instanceof AbstractCompile } as List<AbstractCompile>
 
             String aspectJJrtClasspath = project.configurations.ajc.asPath
-            // TODO
-            String destDir = "$project.buildDir/classes/java/main"
 
-            logI("""
-compiles are :${compiles.collect { it.name }}
+            compiles.forEach { AbstractCompile compile ->
+
+                String sourceVariantName = compile.name
+                        .substring("compile".length())
+                        .toLowerCase()
+
+                String destDir = "$project.buildDir/classes/$sourceVariantName/main"
+
+                logI("""
+current compiles is :$sourceVariantName
 jrtClasspath :$aspectJJrtClasspath
 destDir :$destDir
 """)
-
-            compiles.forEach { AbstractCompile compile ->
 
                 compile.doLast { Task t ->
 
                     logI("${t.name} do last begins.")
 
                     t.ant.taskdef(resource: "org/aspectj/tools/ant/taskdefs/aspectjTaskdefs.properties", classpath: aspectJJrtClasspath)
-//
-//                    t.ant.iajc(
-//                            source: "1.8",
-//                            target: "1.8",
-//                            destDir: "$buildDir/classes/java/main",
-//                            maxmem: "512m",
-//                            fork: "true",
-//                    ) {
-//                        sourceroots {
-//                            sourceSets.main.java.srcDirs.each {
-//                                println " file :$it.absolutePath"
-//                                pathelement(location: it.absolutePath)
-//                            }
-//                        }
-//                    }
+
+                    t.ant.iajc(
+                            source: "1.8",
+                            target: "1.8",
+                            destDir: destDir,
+                            maxmem: "512m",
+                            fork: "true",
+                    ) {
+                        sourceroots {
+
+                            def curSourceDirs = project.sourceSets.main[sourceVariantName].srcDirs
+                                    .findAll { File sourceDir -> sourceDir.exists() }
+
+                            curSourceDirs.each {
+                                pathelement(location: it.absolutePath)
+                            }
+
+                        }
+                    }
 
                 }
 
