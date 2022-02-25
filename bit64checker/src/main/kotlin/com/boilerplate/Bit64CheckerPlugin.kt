5af23e1a45218e7f6a377e7c0491dfbffde4e0e8
yangxiaobin <yangxiaobin@kuaishou.com>
2022-02-25 11:10:16 +0800

    Complete bit64checker plugin.

--- bit64checker/src/main/kotlin/com/boilerplate/Bit64CheckerPlugin.kt
+++ bit64checker/src/main/kotlin/com/boilerplate/Bit64CheckerPlugin.kt
@@ -7,6 +7,11 @@ import java.io.File
 
 class Bit64CheckerPlugin : BasePlugin() {
 
+    // File absolute path.
+    private val v7Set = mutableSetOf<String>()
+    private val v8aSet = mutableSetOf<String>()
+
+
     override fun apply(p: Project) {
         super.apply(p)
 
@@ -22,6 +27,15 @@ class Bit64CheckerPlugin : BasePlugin() {
                         printDir(File(file.absolutePath))
                     }
 
+                    val intersection = v7Set intersect v8aSet
+                    val targets = v7Set - intersection
+
+                    println(" 32 so文件: ${v7Set.size}, ${v7Set.joinToString(separator = "\r\n")}.")
+                    println("----------------------------------------- \r\n")
+                    println(" 64 so文件: ${v8aSet.size}, ${v8aSet.joinToString(separator = "\r\n")}.")
+                    println("----------------------------------------- \r\n")
+                    println(" 未适配 64 位so文件: ${targets.size}, ${targets.joinToString(separator = "\r\n")}.")
+
                     println("---------- ${t.name} 查找so文件归属结束 ---------")
                 }
             }
@@ -36,7 +50,20 @@ class Bit64CheckerPlugin : BasePlugin() {
             if (file.isDirectory) {
                 file.listFiles().forEach { printDir(it) }
             } else if (file.absolutePath.endsWith(".so")) {
-                println("so文件路径: $file.absolutePath")
+
+                // /Users/yangxiaobin/.gradle/caches/transforms-2/files-2.1/497341f4f6b8a19f92c24ab984c1f794/jetified-tunnel-release/jni/armeabi-v7a/libwg-go.so
+
+                // 去掉 armeabi-v7a 或者 arm64-v8a
+                if (file.absolutePath.contains("armeabi-v7a")) {
+                    val truncatedPath = file.absolutePath.replace("/armeabi-v7a", "")
+                    v7Set += truncatedPath
+                }
+
+                if (file.absolutePath.contains("arm64-v8a")) {
+                    val truncatedPath = file.absolutePath.replace("/arm64-v8a", "")
+                    v8aSet += truncatedPath
+                }
+
             }
         }
     }
