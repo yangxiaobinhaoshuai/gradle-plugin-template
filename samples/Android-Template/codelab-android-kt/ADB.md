90f34cf8d24c6d519a104a9f6ad6af0bf1462bc9
yangxiaobin <yangxiaobin@kuaishou.com>
2022-08-23 16:53:14 +0800

    Remove submodule android-template.

--- samples/Android-Template/codelab-android-kt/ADB.md
+++
@@ -1,6 +0,0 @@
-
-
-# Common Adb commands
-
-1. 
-adb shell setprop debug.layout true
--- samples/Android-Template/codelab-android-kt/src/main/java/me/yangxiaobin/android/kotlin/codelab/ext/ContextExt.kt
+++
@@ -1,7 +0,0 @@
-package me.yangxiaobin.android.kotlin.codelab.ext
-
-import android.content.Context
-import android.view.LayoutInflater
-
-
-val Context.inflater: LayoutInflater get() = android.view.LayoutInflater.from(this)
