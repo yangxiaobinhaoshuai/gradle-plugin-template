e888e7adb339babdc60927d017a76e644f77c67a
yangxiaobin <yangxiaobin@kuaishou.com>
2022-08-23 16:28:01 +0800

    Fine tune jvm logger api.

---
+++ logger-jvm/README.md
@@ -0,0 +1,5 @@
+
+
+// 如果有远程编译，注意去掉 
+publish : ./gradlew publishJvmLoggerPublicationToMavenRepository 
+
--- logger-jvm/src/main/kotlin/me/yangxiaobin/logger/elements/LogPrinterLogElement.kt
+++ logger-jvm/src/main/kotlin/me/yangxiaobin/logger/elements/LogPrinterLogElement.kt
@@ -1,8 +1,10 @@
 package me.yangxiaobin.logger.elements
 
-import me.yangxiaobin.logger.domain.AbsKey
 import me.yangxiaobin.logger.domain.AbsDomainElement
+import me.yangxiaobin.logger.domain.AbsKey
 import me.yangxiaobin.logger.uitlity.LogPrinter
+
+open class LogPrinterDelegate(private val printer: LogPrinter) : LogPrinter by printer
 
 data class LogPrinterLogElement(val logPrinter: LogPrinter) : AbsDomainElement(LogPrinterLogElement) {
 
