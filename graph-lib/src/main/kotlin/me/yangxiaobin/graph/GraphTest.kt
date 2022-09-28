6c0ea84ce3f9632564c665ac379cff19a4777ce7
yangxiaobin <yangxiaobin@kuaishou.com>
2022-09-28 17:50:24 +0800

    Add path usecase.

--- graph-lib/src/main/kotlin/me/yangxiaobin/graph/GraphTest.kt
+++ graph-lib/src/main/kotlin/me/yangxiaobin/graph/GraphTest.kt
@@ -1,5 +1,6 @@
 package me.yangxiaobin.graph
 
+import me.yangxiaobin.graph.usecase.Path
 import me.yangxiaobin.graph.usecase.Search
 
 fun graphLog(message: String) = println(message)
@@ -20,13 +21,20 @@ class GraphTest {
                 .addVertexPair(0, 3)
                 .addVertexPair(0, 4)
 
+
                 .addVertexPair(5, 6)
                 .addVertexPair(5, 7)
                 .addVertexPair(5, 8)
                 .addVertexPair(5, 9)
 
+                .addVertexPair(3,10)
+                .addVertexPair(3,12)
+                .addVertexPair(10,11)
+
 
             val search = Search(simpleGraph, 4.toSimpleVertex())
+
+            val path = Path(simpleGraph, 10.toSimpleVertex())
 
             graphLog("graph toAdjString: ${simpleGraph.toAdjString()}")
 
@@ -35,6 +43,14 @@ class GraphTest {
                 search result : 
                 isMarked :${search.isConnected(5.toSimpleVertex())}
                 count :${search.getConnectedCount()}
+            """.trimIndent()
+            )
+
+            graphLog(
+                """
+                path result:
+                hasPathTo: ${path.hasPathTo(6.toSimpleVertex())}
+                path : ${path.pathTo(7.toSimpleVertex())}
             """.trimIndent()
             )
         }
--- graph-lib/src/main/kotlin/me/yangxiaobin/graph/usecase/Path.kt
+++ graph-lib/src/main/kotlin/me/yangxiaobin/graph/usecase/Path.kt
@@ -3,10 +3,53 @@ package me.yangxiaobin.graph.usecase
 import me.yangxiaobin.graph.Graph
 
 
-/**
- * 获取两个顶点的路径
- */
-fun <V, E> Graph<V, E>.pathBetween(v: V, s: V): Iterable<V> {
+class Path<V, E>(graph: Graph<V, E>, target: V) {
 
-    return emptyList()
+    private val marked = mutableMapOf<V, Boolean>()
+
+    /**
+     * this to parent
+     */
+    private val parentMap = mutableMapOf<V,V>()
+
+    init {
+        dfs(graph, target)
+    }
+
+    private fun dfs(graph: Graph<V, E>, target: V) {
+        marked[target] = true
+
+        for (v in graph.getAdjacentVertexes(target)) {
+            if (marked[v] != true) {
+                parentMap[v] = target
+                dfs(graph, v)
+            }
+        }
+    }
+
+    /**
+     * 两点间是否存在路径
+     */
+    fun hasPathTo(v: V): Boolean = marked.getOrDefault(v, false)
+
+    /**
+     * 获取两个顶点的路径
+     */
+    fun pathTo(v: V): Iterable<V> {
+        if (!hasPathTo(v)) return emptyList()
+
+        val actualPath = mutableListOf<V>()
+
+        var parent:V? = parentMap[v]
+
+        while (parent != null) {
+            actualPath += parent
+            parent = parentMap[parent]
+        }
+
+        if (actualPath.isNotEmpty()) actualPath.add(0,v)
+
+        return actualPath.reversed()
+    }
+
 }
